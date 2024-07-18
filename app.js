5502b66ef6d6e5ae42104a6f318eb435fc8c1238
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-07-18 21:33:46 +0530

    building

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+//npx nodemon app.js is the command to run
 const express = require('express');
 const app = express();
+//This is the defauly code to configure sockket.io
 const http = require('http');
 const socketio = require('socket.io');
 const server = http.createServer(app);
