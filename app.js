1468648859d72f1f1619e0cf358e1059e0d03cf6
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-07-19 00:35:18 +0530

    styling added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const http = require('http');
 const socketio = require('socket.io');
 const server = http.createServer(app);
 const io = socketio(server);
+app.set("view engine", 'ejs');
+app.set(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res){
     res.send('hello')
